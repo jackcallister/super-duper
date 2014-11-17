871f0c86f5af680f817dc8eab6f8b93843bc1463
Jack Callister <jarsbe@gmail.com>
2014-11-18 08:41:15 +1300

    Added a create end point.

--- app/controllers/api/meals_controller.rb
+++ app/controllers/api/meals_controller.rb
@@ -4,11 +4,29 @@ class API::MealsController < ApplicationController
   end
 
   def create
+    @meal = Meal.new(meal_params)
+
+    if @meal.save
+      render json: @meal
+    else
+      render json: { success: false }, status: 500
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+private
+
+  def meal_params
+    json_params = ActionController::Parameters.new( ActiveSupport::JSON.decode(params[:meal]) )
+    json_params.permit(permitted_meal_params)
+  end
+
+  def permitted_job_params
+    [:name, ingredients_attributes: [:name]]
   end
 end
