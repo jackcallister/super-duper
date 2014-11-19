43ce3fefd9f59ef3e7b158e2e0a6e2dc875c8d53
jarsbe <jarsbe@gmail.com>
2014-11-19 21:09:02 +1300

    Allow for multiple ingredient inputs.

--- app/assets/javascripts/components/meal-form.js.jsx
+++ app/assets/javascripts/components/meal-form.js.jsx
@@ -2,15 +2,16 @@ var MealForm = React.createClass({
 
   createMeal: function(e) {
     e.preventDefault();
+    var t = this;
     var mealName = this.refs["meal-name"].getDOMNode().value.trim();
-    var ingredientName = this.refs["ingredient-name"].getDOMNode().value.trim();
+    var ingredientAttributes = this.state.ingredients.map(function(ingredient, index) {
+      return { name: t.refs["ingredient-name-" + index].getDOMNode().value.trim() }
+    });
 
     var formData = {
       meal: {
         name: mealName,
-        ingredients_attributes: [{
-          name: ingredientName
-        }]
+        ingredients_attributes: ingredientAttributes
       }
     }
 
@@ -25,12 +26,14 @@ var MealForm = React.createClass({
     });
 
     this.refs["meal-name"].getDOMNode().value = "";
-    this.refs["ingredient-name"].getDOMNode().value = "";
-
-    this.handleToggle();
+    this.state.ingredients.forEach(function(ingredient, index) {
+      t.refs["ingredient-name-" + index].getDOMNode().value = "";
+    });
+    
+    this.didToggle();
   },
 
-  handleToggle: function(e) {
+  didToggle: function(e) {
     this.setState({
       visible: !this.state.visible
     });
@@ -38,28 +41,49 @@ var MealForm = React.createClass({
 
   getInitialState: function() {
     return {
-      visible: false
+      visible: false,
+      ingredients: [this.defaultIngredient()]
     };
   },
 
+  defaultIngredient: function() {
+    return { name: "" }
+  },
+
+  addIngredientField: function(e) {
+    e.preventDefault();
+    var ingredients = this.state.ingredients;
+    ingredients.push(this.defaultIngredient());
+    
+    this.setState({
+      ingredients: ingredients
+    });
+  },
+
   componentDidMount: function() {
-    document.addEventListener('toggleModal', this.handleToggle);
+    document.addEventListener('toggleModal', this.didToggle);
   },
 
   componentWillUnmount: function() {
-    document.removeEventListener('toggleModal', this.handleToggle);
+    document.removeEventListener('toggleModal', this.didToggle);
   },
 
   render: function() {
     var visibleKlass = this.state.visible ? "modal-active" : "modal-inactive";
+   
+    var ingredientInputs = this.state.ingredients.map(function(ingredient, index) {
+      var ref = "ingredient-name-" + index;
+      return (<input type="text" ref={ref} placeholder="Ingredient" key={index} />);
+    });
 
     return (
       <div>
-        <div id="modal-overlay" className={visibleKlass} onClick={this.handleToggle}></div>
+        <div id="modal-overlay" className={visibleKlass} onClick={this.didToggle}></div>
         <div ref="modal" id="modal" className={visibleKlass}>
           <form ref="form" onSubmit={this.createMeal}>
             <input type="text" ref="meal-name" placeholder="Meal name" />
-            <input type="text" ref="ingredient-name" placeholder="Ingredient" />
+            {ingredientInputs}
+            <button onClick={this.addIngredientField} disable>Add ingredient</button>
             <input type="submit" value="Add Meal"/>
           </form>
         </div>
