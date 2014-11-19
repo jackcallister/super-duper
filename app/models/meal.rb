f7dd9e6682c2511b54828cfd7c2669d54d8108fc
jarsbe <jarsbe@gmail.com>
2014-11-19 21:08:42 +1300

    Make has many dependent.

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,5 +1,5 @@
 class Meal < ActiveRecord::Base
-  has_many :ingredients
+  has_many :ingredients, dependent: :destroy
   accepts_nested_attributes_for :ingredients
   validate :name, presence: true
 end
