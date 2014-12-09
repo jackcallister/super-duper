e00ce84f2b405eca182df26d77077636e5455ed0
jarsbe <jarsbe@gmail.com>
2014-12-09 17:34:53 +1300

    Temp turn off.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+  # protect_from_forgery with: :null_session
 
   before_filter :authenticate_user!
 
