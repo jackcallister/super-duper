33ec75bfd61ea9536f4bc6a2d31725c405dc8277
jarsbe <jarsbe@gmail.com>
2014-12-10 23:32:20 +1300

    Webpack bundle.

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -143,6 +143,7 @@
 
 	var React = __webpack_require__(2),
 	    ReactCSSTransitionGroup = React.addons.CSSTransitionGroup,
+	    ReactTransitionGroup = React.addons.TransitionGroup,
 	    MealButton = __webpack_require__(9);
 
 
@@ -175,7 +176,7 @@
 	    return (
 	      React.createElement("div", {className: "meal-picker"}, 
 	        React.createElement("h1", null, "Meal Picker"), 
-	        React.createElement(ReactCSSTransitionGroup, {transitionName: "pop-out", transitionEnter: false}, 
+	        React.createElement(ReactCSSTransitionGroup, {transitionEnter: false, transitionName: "pop-out"}, 
 	          mealButtons
 	        ), 
 	        React.createElement("button", {onClick: this.toggleModal}, "Add")
@@ -454,12 +455,46 @@
 
 	  onDelete: function(e) {
 	    e.stopPropagation();
-	    var result = confirm('Are you sure?');
+	    // var result = confirm('Are you sure?');
 	    
-	    if (result) {
+	    // if (result) {
 	      this.props.onDelete();  
-	    }
+	    // }
 	  },
+
+	  // 1. Components load for the first time.
+	  // 2. An action cause the node to be removed.
+
+	  // 1. Element will enter the DOM
+	  // componentWillEnter: function(callback) {
+	  //   callback();
+	  //   console.log('will enter');
+	  // },
+
+	  // // 1. Element is now in the DOM
+	  // componentDidEnter: function() {
+	  //   console.log('did enter');
+	  // },
+
+	  // // 2. An action has caused this element to be removed from DOM
+	  // //    it will now leave.
+
+	  // // Here is a chance to animate other nodes.
+	  // // 
+	  // // Get the next node and animate it into the position  
+	  // //
+	  // componentWillLeave: function(callback) {
+	  //   console.log('will leave');
+
+	  //   $(this.getDOMNode()).next().addClass('moving');
+
+	  //   callback();
+	  // },
+
+	  // // 2. The component has now left the DOM
+	  // componentDidLeave: function(){
+	  //   console.log('did leave'); 
+	  // },
 
 	  render: function() {
 	    return (
