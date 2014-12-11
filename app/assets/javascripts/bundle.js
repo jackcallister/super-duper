821064fe8f43331bf138aa4b1fbfd3d53dcab130
jarsbe <jarsbe@gmail.com>
2014-12-11 22:15:34 +1300

    Bundle.

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -219,12 +219,6 @@
 	    document.dispatchEvent(event);
 	  },
 
-	  getInitialState: function() {
-	    return {
-	      nextButtonIds: []
-	    };
-	  },
-
 	  render: function() {
 	    var mealButtons = [];
 
@@ -511,41 +505,54 @@
 
 	var MealButton = React.createClass({displayName: 'MealButton',
 
-	  getNextGridItemIds: function() {
-	    var nextGridItems = $(this.getDOMNode()).nextAll('.grid-item');
-	    var nextGridItemsIds = [$(this.getDOMNode()).data('reactid')];
-
-	    nextGridItems.toArray().forEach(function(gridItem, index){
-	      nextGridItemsIds.push($(gridItem).data('reactid'));
-	    });
-
-	    return nextGridItemsIds;
-	  },
-
 	  onDelete: function(e) {
 	    e.stopPropagation();
-
-	    var result = confirm('Are you sure?');
-
-	    if (result) {
-	      var nextGridItemIds = this.getNextGridItemIds();
-
-	      this.props.onDelete();
-	      this.props.onReflowGrid(nextGridItemIds);
-	    }
+	    // var result = confirm('Are you sure?');
+	    
+	    // if (result) {
+	      this.props.onDelete();  
+	    // }
 	  },
 
-	  getInitialState: function() {
-	    return {
-	      nextMealButtonIds: []
-	    };
-	  },
+	  // 1. Components load for the first time.
+	  // 2. An action cause the node to be removed.
+
+	  // 1. Element will enter the DOM
+	  // componentWillEnter: function(callback) {
+	  //   callback();
+	  //   console.log('will enter');
+	  // },
+
+	  // // 1. Element is now in the DOM
+	  // componentDidEnter: function() {
+	  //   console.log('did enter');
+	  // },
+
+	  // // 2. An action has caused this element to be removed from DOM
+	  // //    it will now leave.
+
+	  // // Here is a chance to animate other nodes.
+	  // // 
+	  // // Get the next node and animate it into the position  
+	  // //
+	  // componentWillLeave: function(callback) {
+	  //   console.log('will leave');
+
+	  //   $(this.getDOMNode()).next().addClass('moving');
+
+	  //   callback();
+	  // },
+
+	  // // 2. The component has now left the DOM
+	  // componentDidLeave: function(){
+	  //   console.log('did leave'); 
+	  // },
 
 	  render: function() {
 	    return (
-	      React.createElement("div", {className: "grid-item meal-button", onClick: this.props.onSelect}, 
+	      React.createElement("div", {className: "meal-button-wrapper", onClick: this.props.onSelect}, 
 	        React.createElement("div", {className: "meal-button-bar"}), 
-	        React.createElement("div", {className: "meal-button-label"}, 
+	        React.createElement("div", {className: "meal-button"}, 
 	          React.createElement("span", null, this.props.name), 
 	          React.createElement("i", {className: "icon-delete", onClick: this.onDelete})
 	        )
