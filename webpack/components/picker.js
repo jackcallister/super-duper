cf6bcbce3ddc1b96b3ce6ed717bac8ec2e42291d
jarsbe <jarsbe@gmail.com>
2015-02-18 15:08:04 +1300

    Wire picker up to local meal store.

--- webpack/components/picker.js
+++ webpack/components/picker.js
@@ -1,18 +1,27 @@
 var React = require('react/addons'),
     MealStore = require('../stores/meal-store'),
+    LocalMealStore = require('../stores/local-meal-store'),
     MealButton = require('./picker/meal-button');
+    DisabledMealButton = require('./picker/disabled-meal-button');
 
 function getStateFromStores() {
   return {
-    meals: MealStore.getAll()
+    meals: MealStore.getAll(),
+    localMeals: LocalMealStore.getAll()
   }
 }
 
 var MealPicker = React.createClass({
 
   _renderMealButtons: function() {
-    return this.state.meals.map(function(meal) {
-      return <MealButton meal={meal} key={'meal-button-' + meal.id} />;
+    return this.state.meals.map(function(meal, index) {
+      return <MealButton meal={meal} key={'meal-button-' + index} />;
+    });
+  },
+
+  _renderSavingMealButtons: function() {
+    return this.state.localMeals.map(function(meal, index) {
+      return <DisabledMealButton meal={meal} key={'saving-meal-button-' + index} />;
     });
   },
 
@@ -22,10 +31,12 @@ var MealPicker = React.createClass({
 
   componentDidMount: function() {
     MealStore.addChangeListener(this._onChange);
+    LocalMealStore.addChangeListener(this._onChange);
   },
 
   componentWillUnmount: function() {
     MealStore.removeChangeListener(this._onChange);
+    LocalMealStore.removeChangeListener(this._onChange);
   },
 
   getInitialState: function() {
@@ -34,13 +45,12 @@ var MealPicker = React.createClass({
 
   render: function() {
     var mealButtons = this._renderMealButtons();
+    var localMealButtons = this._renderSavingMealButtons();
 
     return (
       <div className='meal-picker'>
         {mealButtons}
-        <div className='grid-item'>
-          <button>Add Meal</button>
-        </div>
+        {localMealButtons}
       </div>
     );
   }
