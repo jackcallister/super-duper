81cf512dec73d200a47a7bd1a1ae00c6105888da
jarsbe <jarsbe@gmail.com>
2015-02-19 11:45:41 +1300

    Add a cache ID.

--- webpack/actions/meal-action-creators.js
+++ webpack/actions/meal-action-creators.js
@@ -1,14 +1,18 @@
 var AppDispatcher = require('../dispatcher/app');
 var ActionTypes = require('../constants/app').ActionTypes;
 var WebUtils = require('../utils/web-utils');
+var MealUtils = require('../utils/meal-utils');
 
 module.exports = {
   createMeal: function(meal) {
+    var cacheId = MealUtils.generateCacheId();
+
     AppDispatcher.handleViewAction({
       type: ActionTypes.CREATE_MEAL,
-      meal: meal
+      meal: meal,
+      cacheId: cacheId
     });
 
-    WebUtils.createMeal(meal);
+    WebUtils.createMeal(meal, cacheId);
   }
 }
---
+++ webpack/utils/meal-utils.js
@@ -0,0 +1,5 @@
+module.exports = {
+  generateCacheId: function() {
+    return new Date().getTime();
+  }
+}
